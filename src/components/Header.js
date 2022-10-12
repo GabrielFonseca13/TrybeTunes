fb94d927392135a2c076d7e233455dce1bce3d96
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-12 00:58:02 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -25,6 +26,9 @@ class Header extends React.Component {
             <h3 data-testid="header-user-name">{userName.name}</h3>
           )
         }
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
