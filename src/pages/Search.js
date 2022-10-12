165733103e19592011decf3c496627b0c5cd3e7a
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-12 01:19:07 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,60 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    bandArtist: '',
+    btnDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => this.buttonValidation());
+  };
+
+  buttonValidation = () => {
+    const { bandArtist, btnDisabled } = this.state;
+    const minArtistSize = 2;
+    if (bandArtist.length >= minArtistSize) {
+      this.setState({
+        btnDisabled: false,
+      });
+    } else {
+      this.setState({
+        btnDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { bandArtist, btnDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>SEARCH</h1>
+        <form>
+          <label htmlFor="bandArtist">
+            Banda/Artista:
+            <input
+              id="bandArtist"
+              name="bandArtist"
+              type="text"
+              data-testid="search-artist-input"
+              value={ bandArtist }
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ btnDisabled }
+              // onClick={}
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
