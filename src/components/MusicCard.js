cfbff119d8230a8c1c9321d1d8934e8d307a1d7e
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-13 09:53:20 -0300

    requisito 13

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import Header from './Header';
+// import React from 'react';
+// import Header from './Header';
 
-class MusicCard extends React.Component {
-  render() {
-    const { tracks } = this.props;
-    return (
-      <div>
-        <Header />
-        <h1>MusicCard</h1>
-        <div>
-          <ul>
-            {tracks.map((music, index) => (
-              <li key={ index }>
-                <img src={ music.artworkUrl100 } alt="" />
-                <h3 data-testid="album-name">{music.collectionName}</h3>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
+// class MusicCard extends React.Component {
+//   render() {
+//     const { tracks } = this.props;
+//     return (
+//       <div>
+//         <Header />
+//         <h1>MusicCard</h1>
+//         <div>
+//           <ul>
+//             {tracks.map((music, index) => (
+//               <li key={ index }>
+//                 <img src={ music.artworkUrl100 } alt="" />
+//                 <h3 data-testid="album-name">{music.collectionName}</h3>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default MusicCard;
+// export default MusicCard;
