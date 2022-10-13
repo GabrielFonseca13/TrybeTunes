ef4769cd0b8c64a0a467525ee65a05ab1904e221
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-13 01:23:32 -0300

    requisito 6

---
+++ src/components/CardAlbum.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class CardAlbum extends React.Component {
+  render() {
+    const { albuns, artistName } = this.props;
+
+    return (
+      <div>
+        <h3>{`Resultado de álbuns de: ${artistName}`}</h3>
+        <div>
+          {albuns.map((album) => (
+            <div key={ album.collectionId }>
+              <div>
+                <img src={ album.artworkUrl100 } alt={ album.artistName } />
+              </div>
+              <div>
+                <h4>{`Artista :${album.artistName}`}</h4>
+                <h5>{`coleção :${album.collectionName}`}</h5>
+                <Link
+                  to={ `/album/${album.collectionId}` }
+                  data-testid={ `link-to-album-${album.collectionId}` }
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+CardAlbum.propTypes = {
+  albuns: PropTypes.arrayOf,
+}.isRequired;
+
+export default CardAlbum;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import CardAlbum from '../components/CardAlbum';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   state = {
     bandArtist: '',
+    inputSearch: '',
     btnDisabled: true,
+    loading: false,
+    albuns: [],
+    callApi: false,
   };
 
   handleChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [name]: value,
-    }, () => this.buttonValidation());
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => this.buttonValidation(),
+    );
   };
 
   buttonValidation = () => {
@@ -29,33 +39,63 @@ class Search extends React.Component {
     }
   };
 
+  clickHandleSearch = async () => {
+    const { bandArtist } = this.state;
+    this.setState({
+      inputSearch: bandArtist,
+      bandArtist: '',
+      loading: true,
+    });
+    const response = await searchAlbumsAPI(bandArtist);
+    this.setState({
+      albuns: response,
+      btnDisabled: true,
+      callApi: true,
+      loading: false,
+    });
+  };
+
   render() {
-    const { bandArtist, btnDisabled } = this.state;
+    const { bandArtist, btnDisabled, loading, albuns, callApi, inputSearch } = this.state;
     return (
       <div data-testid="page-search">
-        <Header />
         <h1>SEARCH</h1>
-        <form>
-          <label htmlFor="bandArtist">
-            Banda/Artista:
-            <input
-              id="bandArtist"
-              name="bandArtist"
-              type="text"
-              data-testid="search-artist-input"
-              value={ bandArtist }
-              onChange={ this.handleChange }
-            />
-            <button
-              type="button"
-              data-testid="search-artist-button"
-              disabled={ btnDisabled }
-              // onClick={}
-            >
-              Pesquisar
-            </button>
-          </label>
-        </form>
+        <Header />
+        {
+          loading
+            ? <Loading />
+            : (
+              <form>
+                <label htmlFor="bandArtist">
+                  Banda/Artista:
+                  <input
+                    id="bandArtist"
+                    name="bandArtist"
+                    type="text"
+                    data-testid="search-artist-input"
+                    value={ bandArtist }
+                    onChange={ this.handleChange }
+                  />
+                  <button
+                    type="button"
+                    data-testid="search-artist-button"
+                    disabled={ btnDisabled }
+                    onClick={ this.clickHandleSearch }
+                  >
+                    Pesquisar
+                  </button>
+                </label>
+              </form>
+            )
+        }
+
+        {callApi && albuns.length > 0
+          ? <CardAlbum albuns={ albuns } artistName={ inputSearch } />
+          : (
+            <div>
+              <h3>Nenhum álbum foi encontrado</h3>
+            </div>
+          )}
       </div>
     );
   }
